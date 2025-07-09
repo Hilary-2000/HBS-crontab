7d6c5cf2f4e4b4067760061f783dbb4547ec5be2
Hillary <hilaryme45@gmail.com>
2025-07-09 17:39:45 +0300

    Add router os changes

--- sms_reminder.php
+++ sms_reminder.php
@@ -8,6 +8,21 @@
  * you will get notified again after 10 messages three time at the 110, 120 and the 120 mark to emphasize the message
  */
 
+	// allow only certain ip addresses
+	$allowed_ip_address = "172.71.178.94";
+	$server_ip_address = $_SERVER['REMOTE_ADDR'] ?? 'UNKNOWN';
+	if (php_sapi_name() === 'cli') {
+		// Running from CLI (Terminal)
+		$server_ip_address = '172.71.178.94'; // Assume local execution
+	} else {
+		// Running from Web
+		$server_ip_address = $_SERVER['REMOTE_ADDR'] ?? 'UNKNOWN';
+	}
+	if ($allowed_ip_address !== $server_ip_address) {
+		echo "Server ip address not allowed \"".$server_ip_address."\"";
+		return 0;
+	}
+
 //  connect to the database
 	// Connect REMOTE
 	$dbname = 'my_isp';
--- syncdb.php
+++ syncdb.php
@@ -5,6 +5,21 @@
 	// file_put_contents($filename,$data,FILE_APPEND);
 	date_default_timezone_set('Africa/Nairobi');
 
+	// allow only certain ip addresses
+	$allowed_ip_address = "172.71.178.94";
+	$server_ip_address = $_SERVER['REMOTE_ADDR'] ?? 'UNKNOWN';
+	if (php_sapi_name() === 'cli') {
+		// Running from CLI (Terminal)
+		$server_ip_address = '172.71.178.94'; // Assume local execution
+	} else {
+		// Running from Web
+		$server_ip_address = $_SERVER['REMOTE_ADDR'] ?? 'UNKNOWN';
+	}
+	if ($allowed_ip_address !== $server_ip_address) {
+		echo "Server ip address not allowed \"".$server_ip_address."\"";
+		return 0;
+	}
+
 	// get connection local database and get the values of the users that are due that minute
     // LOCAL
 	$dbname = 'hypbits';
